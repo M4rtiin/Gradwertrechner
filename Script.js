85caeaf59534a27dfed3153dbd7381e6d07a5cf8
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2020-06-04 20:40:38 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,16 +1,29 @@
 
-function Berechnen(){
 
+function Berechnen(){
+//Eingebefelder
   var Umfang = document.getElementById("Umfang").value;
   var werteingabe = document.getElementById("Gradwerte").value;
-
+//Berechnung
   var multi = Umfang/360;
   var rechner = werteingabe * multi;
-
+  speicher.push(rechner);
+  gradspeicher.push(werteingabe);
+  var diverenz = rechner - speicher[speicher.length-2];
+//Umfanganzeige
   document.getElementById("U").innerHTML ="U="+ Umfang +"mm";
-
+//Gradanzeige
   var div = document.getElementById("S");
   var createE = document.createElement("p");
   createE.innerHTML = werteingabe+"° = "+ rechner+"mm";
-  div.appendChild(createE)
+  div.appendChild(createE);
+//diverenzanzeige
+  var createB = document.createElement("p");
+  createB.innerHTML = gradspeicher[gradspeicher.length-2]+"° --> " + werteingabe + "° = " + diverenz + "mm";
+  div.appendChild(createB);
+ console.log(speicher);
+ console.log(speicher.length);
 }
+//Speicher für diverenzberechnung
+  var speicher = [];
+  var gradspeicher = [];
