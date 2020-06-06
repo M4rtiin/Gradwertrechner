7135d9d074e168da51c9e3152f411a8ea18d7b8c
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2020-06-06 14:07:59 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -14,22 +14,36 @@ function Berechnen(){
   var divergebnis = Math.round(diverenz*10)/10;
 //Umfanganzeige
   document.getElementById("U").innerHTML ="U="+ Umfang +"mm";
-//diverenzanzeige
-  var div = document.getElementById("S");
-  var createB = document.createElement("p");
-  createB.innerHTML = gradspeicher[gradspeicher.length-2]+"° --> " + werteingabe + "° = " + divergebnis + "mm";
-  div.appendChild(createB);
+
+ if (speicher.length===1) {
 //Gradanzeige
-  var createE = document.createElement("p");
-  createE.innerHTML = werteingabe + "° = " + ergebnis + "mm";
-  div.appendChild(createE);
+   var div = document.getElementById("S");
+   var createE = document.createElement("p");
+   createE.innerHTML = werteingabe + "° = " + ergebnis + "mm";
+   div.appendChild(createE);
 //Abgrenzung
-  var createA = document.createElement("p");
-  createA.innerHTML = "---------------------";
-  div.appendChild(createA);
+   var createA = document.createElement("p");
+   createA.innerHTML = "---------------------";
+   div.appendChild(createA);
+    }
+ else {
+//diverenzanzeige
+    var div = document.getElementById("S");
+    var createB = document.createElement("p");
+    createB.innerHTML = gradspeicher[gradspeicher.length-2]+"° --> " + werteingabe + "° = " + divergebnis + "mm";
+    div.appendChild(createB);
+//Gradanzeige
+    var createE = document.createElement("p");
+    createE.innerHTML = werteingabe + "° = " + ergebnis + "mm";
+    div.appendChild(createE);
+//Abgrenzung
+    var createA = document.createElement("p");
+    createA.innerHTML = "---------------------";
+    div.appendChild(createA);
+ }
 //debug
-  console.log(speicher);
-  console.log(speicher.length);
+    console.log(speicher);
+    console.log(speicher.length);
 }
 //Speicher für Diverenzberechnung und Anzeige
   var speicher = [];
