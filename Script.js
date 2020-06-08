9227dba0455ed3e9bbdde7beb5251f25543e045d
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2020-06-08 23:39:03 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -2,10 +2,10 @@
 //Gradwertrechner
 
 function Berechnen(){
-//Eingebefelder
+  //Eingebefelder
   var Umfang = document.getElementById("Umfang").value;
   var werteingabe = document.getElementById("Gradwerte").value;
-//Berechnung
+  //Berechnung
   var multi = Umfang/360;
   var rechner = werteingabe * multi;
   var ergebnis = Math.round(rechner*10)/10;
@@ -13,45 +13,68 @@ function Berechnen(){
   gradspeicher.push(werteingabe);
   var diverenz = ergebnis - speicher[speicher.length-2];
   var divergebnis = Math.round(diverenz*10)/10;
-//Umfanganzeige
+  //Umfanganzeige
   document.getElementById("U").innerHTML ="U="+ Umfang +"mm";
 
  if (speicher.length===1) {
-//Gradanzeige
+   //Gradanzeige
    var div = document.getElementById("Anzeige");
    var createE = document.createElement("p");
    createE.innerHTML = werteingabe + "° = " + ergebnis + "mm";
    div.appendChild(createE);
-//Abgrenzung
+   //Abgrenzung
    var createA = document.createElement("p");
    createA.innerHTML = "---------------------";
    div.appendChild(createA);
     }
  else {
-//diverenzanzeige
+    //diverenzanzeige
     var div = document.getElementById("Anzeige");
     var createB = document.createElement("p");
     createB.innerHTML = gradspeicher[gradspeicher.length-2]+"° --> " + werteingabe + "° = " + divergebnis + "mm";
     div.appendChild(createB);
-//Gradanzeige
+    //Gradanzeige
     var createE = document.createElement("p");
     createE.innerHTML = werteingabe + "° = " + ergebnis + "mm";
     div.appendChild(createE);
-//Abgrenzung
+    //Abgrenzung
     var createA = document.createElement("p");
     createA.innerHTML = "---------------------";
     div.appendChild(createA);
  }
-//debug
+    //debug
     console.log(speicher);
     console.log(speicher.length);
 }
-//Speicher für Diverenzberechnung und Anzeige
+  //Speicher für Diverenzberechnung und Anzeige
   var speicher = [];
   var gradspeicher = [];
 
 //Bohrerdrehzahlrechner
 
 function drehzahlberechnung(){
-  
+  //Eingabefeld
+  var durchmesser = document.getElementById("bohrereingabe").value;
+  var edelstahl = document.getElementById("va").checked;
+  var stahl = document.getElementById("h2").checked;
+  var durchmessera = document.getElementById("durchmesseranzeige");
+  var drehzahla = document.getElementById("bohrerdrehzahlanzeige");
+  //Berechnung
+  var vadrehzahl = Math.round(10*1000)/(durchmesser*3.14);
+  var h2drehzahl = Math.round(20*1000)/(durchmesser*3.14);
+  //Anzeige
+  durchmessera.innerHTML = "Bohrerdurchmesser = "+durchmesser;
+  if (edelstahl===true) {
+    drehzahla.innerHTML = "Bohrerdrehzahl = "+vadrehzahl;
+  }
+  else if (stahl===true) {
+    drehzahla.innerHTML = "Bohrerdrehzahl = "+h2drehzahl;
+  }
+  else {
+    drehzahla.innerHTML = "Bitte Material auswählen!";
+  }
+//debug
+console.log(vadrehzahl);
+console.log(h2drehzahl);
+console.log(edelstahl);
 }
