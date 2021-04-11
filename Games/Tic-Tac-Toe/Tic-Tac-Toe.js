6208fecdbe87e4b290cadde5eca7eae462b22ecd
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2021-04-11 17:53:19 +0200

    Add files via upload

--- Games/Tic-Tac-Toe/Tic-Tac-Toe.js
+++ Games/Tic-Tac-Toe/Tic-Tac-Toe.js
@@ -14,20 +14,18 @@ window.onload = function()
   document.getElementById("Anzeige").innerHTML = "Grün ist an der Reihe";
 }
 
-function main ()
+function main()
 {
 
 }
 
-function winner ()
+function winner()
 {
 
 }
 
-function player ()
+function playerAnzeige()
 {
-  counter += 1;
-
   if (counter%2 == 0)
   {
     document.getElementById("Anzeige").innerHTML = "Grün ist an der Reihe";
@@ -35,5 +33,140 @@ function player ()
   else
   {
     document.getElementById("Anzeige").innerHTML = "Blau ist an der Reihe";
+  }
+}
+
+function Bf1()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B1").style.background = "blue";
+    B1 = 2;
+  }
+  else
+  {
+    document.getElementById("B1").style.background = "green";
+    B1 = 1;
+  }
+}
+
+function Bf2()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B2").style.background = "blue";
+    B2 = 2;
+  }
+  else
+  {
+    document.getElementById("B2").style.background = "green";
+    B2 = 1;
+  }
+}
+
+function Bf3()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B3").style.background = "blue";
+    B3 = 2;
+  }
+  else
+  {
+    document.getElementById("B3").style.background = "green";
+    B3 = 1;
+  }
+}
+
+function Bf4()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B4").style.background = "blue";
+    B4 = 2;
+  }
+  else
+  {
+    document.getElementById("B4").style.background = "green";
+    B4 = 1;
+  }
+}
+
+function Bf5()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B5").style.background = "blue";
+    B5 = 2;
+  }
+  else
+  {
+    document.getElementById("B5").style.background = "green";
+    B5 = 1;
+  }
+}
+
+function Bf6()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B6").style.background = "blue";
+    B6 = 2;
+  }
+  else
+  {
+    document.getElementById("B6").style.background = "green";
+    B6 = 1;
+  }
+}
+
+function Bf7()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B7").style.background = "blue";
+    B7 = 2;
+  }
+  else
+  {
+    document.getElementById("B7").style.background = "green";
+    B7 = 1;
+  }
+}
+
+function Bf8()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B8").style.background = "blue";
+    B8 = 2;
+  }
+  else
+  {
+    document.getElementById("B8").style.background = "green";
+    B8 = 1;
+  }
+}
+
+function Bf9()
+{
+  counter += 1;
+  if (counter%2 ==0)
+  {
+    document.getElementById("B9").style.background = "blue";
+    B9 = 2;
+  }
+  else
+  {
+    document.getElementById("B9").style.background = "green";
+    B9 = 1;
   }
 }
