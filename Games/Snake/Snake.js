4d2b86573cf43f8c158434cecfe75980f6cada10
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2022-04-03 19:27:56 +0200

    Add files via upload

--- Games/Snake/Snake.js
+++ Games/Snake/Snake.js
@@ -17,14 +17,14 @@ function Navi() {
 //Snake Game
 
 //Varibel Sammlung
-var score = 0;
-var scoreAlt = 0;
+var score = 1;
+var scoreAlt = 1;
 var freePixelStandort = [0];
-var snakePixelStandort = [
-    [75, 270],
-    [150, 210],
-];
+var snakePixelStandortX = [0,15,15];
+var snakePixelStandortY = [0, 0, 15];
+var loose = false;
 var laufrichtung = 0;
+
 //image Sammlung
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -35,7 +35,10 @@ freePixelimg.src = 'img/freePixel.png';
 
 
 function game() {
-
+    freeBodypixel();
+    snake();
+    draw();
+    bodypixelMatch();
 }
 
 function freeBodypixel() {
@@ -44,19 +47,68 @@ function freeBodypixel() {
         var randomY = Math.ceil(Math.random()*100/5)*15;
         freePixelStandort.pop();
         freePixelStandort.push(randomX, randomY);
-        ctx.drawImage(freePixelimg, freePixelStandort[0], freePixelStandort[1]);
     }
-    else {
-        ctx.drawImage(freePixelimg, freePixelStandort[0], freePixelStandort[1]);
-    }
+    return;
+ 
+    //debug
     console.log(freePixelStandort);
 }
 
 function snake() {
+    if (score == scoreAlt) {
+        snakePixelStandortX.push(snakePixelStandortX[snakePixelStandortX.length - 1] + 15);
+        snakePixelStandortY.push(snakePixelStandortY[snakePixelStandortY.length - 1] + 0);
+        snakePixelStandortX.shift();
+        snakePixelStandortY.shift();
+    }
+    else {
+        snakePixelStandortX.push(snakePixelStandortX[snakePixelStandortX.length - 1] + 15);
+        snakePixelStandortY.push(snakePixelStandortY[snakePixelStandortY.length - 1] + 0);
+        scoreAlt = score;
+    }
 
+    if (snakePixelStandortX[snakePixelStandortX.length - 1] > 300) {
+        snakePixelStandortX.pop();
+        snakePixelStandortX.push(0);
+    }
+    else if (snakePixelStandortX[snakePixelStandortX.length - 1] < 0) {
+        snakePixelStandortX.pop();
+        snakePixelStandortX.push(300);
+    }
+    else if (snakePixelStandortY[snakePixelStandortY.length - 1] > 300) {
+        snakePixelStandortY.pop();
+        snakePixelStandortY.push(0);
+    }
+    else if (snakePixelStandortY[snakePixelStandortY.length - 1] < 0) {
+        snakePixelStandortY.pop();
+        snakePixelStandortY.push(300);
+    }
+    //debug
+    console.log(snakePixelStandortX);
+    console.log(snakePixelStandortY);
 }
 
 function bodypixelMatch() {
+    for (let i = 0; i < snakePixelStandortX; i++) {
+        if (freePixelStandort[0] == snakePixelStandortX[i] && freePixelStandort[1] == snakePixelStandortY[i]) {
+            score++;
+            freePixelStandort.shift();
+            freePixelStandort.shift();
+            freePixelStandort.push(0);
+        }
+    }
+    for (let i = 0; i < snakePixelStandortX.length-1; i++) {
+        if (snakePixelStandortX[snakePixelStandortX.length - 1] == snakePixelStandortX[i] && snakePixelStandortY[snakePixelStandortY.length - 1] == snakePixelStandortY[i]) {
+            loose = true;
+        }
+    }
+}
 
+function draw() {
+    ctx.clearRect(0, 0, 315, 315);
+    for (let i = 0; i < snakePixelStandortX.length; i++) {
+        ctx.drawImage(snakeimg, snakePixelStandortX[i], snakePixelStandortY[i]);
+    }
+    ctx.drawImage(freePixelimg, freePixelStandort[0], freePixelStandort[1]);
 }
 
