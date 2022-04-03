6701351ca0f3b08f905bd507d7f44ffc60548a6a
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2022-04-03 10:55:47 +0200

    Add files via upload

---
+++ Games/Snake/Snake.js
@@ -0,0 +1,18 @@
+
+//Header
+var headerV = true;
+
+function Navi() {
+    if (headerV == true) {
+        document.getElementById("header").style.display = "none";
+        headerV = false;
+    }
+    else {
+        document.getElementById("header").removeAttribute("style");
+        headerV = true;
+    }
+}
+
+
+//Snake Game
+
