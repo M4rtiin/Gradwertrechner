9ea6f3c7035372079b2f374bab6a79efbff95ec7
M4rtiin <64203140+M4rtiin@users.noreply.github.com>
2022-04-04 14:05:13 +0200

    Add files via upload

--- Games/Snake/Snake.js
+++ Games/Snake/Snake.js
@@ -17,13 +17,19 @@ function Navi() {
 //Snake Game
 
 //Varibel Sammlung
-var score = 1;
-var scoreAlt = 1;
+var scoreAnzeige = document.getElementById("score");
+var score = 0;
+var scoreAlt = 0;
 var freePixelStandort = [0];
 var snakePixelStandortX = [0,15,15];
 var snakePixelStandortY = [0, 0, 15];
 var loose = false;
 var laufrichtung = 0;
+var right = 0;
+var left = 0;
+var up = 0;
+var down = 15;
+var test = 0;
 
 //image Sammlung
 var canvas = document.getElementById("canvas");
@@ -34,36 +40,44 @@ const freePixelimg = new Image();
 freePixelimg.src = 'img/freePixel.png';
 
 
+function gameloop() {
+   var interval =  setInterval(game, 1000);
+}
+
 function game() {
-    freeBodypixel();
-    snake();
-    draw();
-    bodypixelMatch();
+        freeBodypixel();
+        snake();
+        bodypixelMatch();
+        draw();
+
+        //debug
+        console.log(freePixelStandort);;
+        console.log(snakePixelStandortX);
+        console.log(snakePixelStandortY);
+        console.log(score);
+     
 }
 
 function freeBodypixel() {
     if (freePixelStandort == 0) {
-        var randomX = Math.ceil(Math.random()*100/5)*15;
-        var randomY = Math.ceil(Math.random()*100/5)*15;
+        var randomX = Math.ceil(Math.random() * 100 / 5) * 15;
+        var randomY = Math.ceil(Math.random() * 100 / 5) * 15;
         freePixelStandort.pop();
         freePixelStandort.push(randomX, randomY);
     }
     return;
- 
-    //debug
-    console.log(freePixelStandort);
 }
 
 function snake() {
     if (score == scoreAlt) {
-        snakePixelStandortX.push(snakePixelStandortX[snakePixelStandortX.length - 1] + 15);
-        snakePixelStandortY.push(snakePixelStandortY[snakePixelStandortY.length - 1] + 0);
+        snakePixelStandortX.push(snakePixelStandortX[snakePixelStandortX.length - 1] + right - left);
+        snakePixelStandortY.push(snakePixelStandortY[snakePixelStandortY.length - 1] + down - up);
         snakePixelStandortX.shift();
         snakePixelStandortY.shift();
     }
     else {
-        snakePixelStandortX.push(snakePixelStandortX[snakePixelStandortX.length - 1] + 15);
-        snakePixelStandortY.push(snakePixelStandortY[snakePixelStandortY.length - 1] + 0);
+        snakePixelStandortX.push(snakePixelStandortX[snakePixelStandortX.length - 1] + right - left);
+        snakePixelStandortY.push(snakePixelStandortY[snakePixelStandortY.length - 1] + down - up);
         scoreAlt = score;
     }
 
@@ -83,13 +97,10 @@ function snake() {
         snakePixelStandortY.pop();
         snakePixelStandortY.push(300);
     }
-    //debug
-    console.log(snakePixelStandortX);
-    console.log(snakePixelStandortY);
 }
 
 function bodypixelMatch() {
-    for (let i = 0; i < snakePixelStandortX; i++) {
+    for (let i = 0; i < snakePixelStandortX.length; i++) {
         if (freePixelStandort[0] == snakePixelStandortX[i] && freePixelStandort[1] == snakePixelStandortY[i]) {
             score++;
             freePixelStandort.shift();
@@ -99,7 +110,7 @@ function bodypixelMatch() {
     }
     for (let i = 0; i < snakePixelStandortX.length-1; i++) {
         if (snakePixelStandortX[snakePixelStandortX.length - 1] == snakePixelStandortX[i] && snakePixelStandortY[snakePixelStandortY.length - 1] == snakePixelStandortY[i]) {
-            loose = true;
+            clearInterval(interval);
         }
     }
 }
@@ -110,5 +121,25 @@ function draw() {
         ctx.drawImage(snakeimg, snakePixelStandortX[i], snakePixelStandortY[i]);
     }
     ctx.drawImage(freePixelimg, freePixelStandort[0], freePixelStandort[1]);
+    scoreAnzeige.innerHTML = "Score: " + score;
 }
 
+/*
+jQuery(document).ready(function () {
+    $(document).on('swipeleft', getLeft);
+    $(document).on('swiperight', getRight);
+
+    function getLeft() {
+        left = 15;
+        right = 0;
+        up = 0;
+        down = 0;
+    }
+    function getRight() {
+        left = 0;
+        right = 15;
+        up = 0;
+        down = 0;
+    }
+});
+*/
